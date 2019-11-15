2b14ced5fe2ee4def2a970f9acf63698fea81500
software3000 <desarrollo@software3000.net>
2019-11-15 18:04:19 -0500

    Creando estructura para Proyecto IntranetPJ e IntranetPJAdmin

---
+++ SistemaReclutamiento/Controllers/IntranetPjAdmin/IntranetPjAdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaReclutamiento.Controllers.IntranetPjAdmin
+{
+    public class IntranetPjAdminController : Controller
+    {
+        // GET: IntranetPjAdmin
+        public ActionResult IntranetPJAdminIndex()
+        {
+            return View();
+        }
+    }
+}
