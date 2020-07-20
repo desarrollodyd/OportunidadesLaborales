26913ee1225e0569cbf7dbd807c220e7fb7ac3b5
Diego Canchari <d.cancharir@gmail.com>
2020-07-20 09:58:33 -0500

    ok

---
+++ SistemaReclutamiento/Entidades/FichaCumplimiento/CumCuestionarioEntidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaReclutamiento.Entidades
+{
+    public class CumCuestionarioEntidad
+    {
+        public string cue_descripcion { get; set; }
+        public string cue_esado { get; set; }
+        public DateTime cue_fecha_act { get; set; }
+        public DateTime cue_fecha_reg { get; set; }
+        public int cue_id { get; set; }
+        public string cue_nombre { get; set; }
+        public int cue_fk_usuario { get; set; }
+    }
+}
---
+++ SistemaReclutamiento/Entidades/FichaCumplimiento/CumUsuRespuestaEntidad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SistemaReclutamiento.Entidades
+{
+    public class CumUsuRespuestaEntidad
+    {
+        public int ure_id { get; set; }
+        public string ure_dni { get; set; }
+        public string ure_respuesta { get; set; }
+        public string ure_tipo { get; set; }
+        public int ure_orden { get; set; }
+        public int ure_calificacion { get; set; }
+        public string ure_estado { get; set; }
+        public int fk_usu_pregunta { get; set; }
+        public int fk_usuario { get; set; }
+    }
+}
