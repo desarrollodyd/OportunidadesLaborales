7a84ca98ddbff06e37db13634930e537fdbace7a
software3000 <desarrollo@software3000.net>
2022-08-02 17:54:41 -0500

    metodo consulta rrhh ofisis

---
+++ SistemaReclutamiento/Controllers/OfisisController.cs
@@ -0,0 +1,32 @@
+﻿using SistemaReclutamiento.Entidades;
+using SistemaReclutamiento.Models;
+using SistemaReclutamiento.Utilitarios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SistemaReclutamiento.Controllers
+{
+    [autorizacion(false)]
+    public class OfisisController : Controller
+    {
+        SQLModel sqlbl = new SQLModel();
+        [HttpGet]
+        public ActionResult PersonaPorDniFechaCese(string dni="")
+        {
+            PersonaSqlEntidad personaSQL = new PersonaSqlEntidad();
+            try
+            {
+                personaSQL = sqlbl.PersonaPorDniFechaCese(dni);
+
+            }
+            catch (Exception ex)
+            {
+            }
+
+            return Json(new {data=personaSQL }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
--- SistemaReclutamiento/Models/SQLModel.cs
+++ SistemaReclutamiento/Models/SQLModel.cs
@@ -1060,5 +1060,94 @@ FROM " + nombre_tabla+" as pago "+
             }
             return (listaempresa: lista, error: error);
         }
+
+        public PersonaSqlEntidad PersonaPorDniFechaCese(string dni)
+        {
+            PersonaSqlEntidad persona = new PersonaSqlEntidad();
+            claseError error = new claseError();
+            string consulta = @"Select top 1
+emp.CO_TRAB, 
+emp.NO_TRAB, 
+emp.NO_APEL_PATE, 
+emp.NO_APEL_MATE, 
+emp.TI_SITU, 
+empresa.CO_EMPR, 
+empresa.DE_NOMB, 
+unidad.CO_UNID, 
+unidad.DE_UNID, 
+sede.CO_SEDE, 
+sede.DE_SEDE, 
+gerencia.CO_DEPA, 
+gerencia.DE_DEPA, 
+area.CO_AREA, 
+area.DE_AREA, 
+grupo.CO_GRUP_OCUP, 
+grupo.DE_GRUP_OCUP, 
+puesto.CO_PUES_TRAB, 
+puesto.DE_PUES_TRAB, 
+emp.FE_INGR_CORP, 
+emp.FE_NACI_TRAB, 
+emp.NU_TLF1, 
+emp.NU_TLF2, 
+emp.NO_DIRE_MAI1, 
+emp.NO_DIRE_TRAB,
+empresa.NU_RUCS,
+trab.FE_CESE_TRAB,
+trab.FE_INGR_EMPR
+from TMTRAB_PERS as emp inner join TMTRAB_CALC as periodo on emp.CO_TRAB=periodo.CO_TRAB 
+inner join TMEMPR as empresa on periodo.CO_EMPR=empresa.CO_EMPR 
+inner join TMUNID_EMPR as unidad on unidad.CO_EMPR=empresa.CO_EMPR and unidad.CO_UNID=periodo.CO_UNID 
+inner join TTSEDE as sede on sede.CO_EMPR=empresa.CO_EMPR and periodo.CO_SEDE=sede.CO_SEDE 
+inner join TTDEPA as gerencia on gerencia.CO_EMPR=empresa.CO_EMPR and periodo.CO_DEPA=gerencia.CO_DEPA 
+inner join TTAREA as area on area.CO_AREA=periodo.CO_AREA and area.CO_EMPR=periodo.CO_EMPR and periodo.CO_DEPA=area.CO_DEPA 
+inner join TTGRUP_OCUP as grupo on grupo.CO_EMPR=empresa.CO_EMPR and grupo.CO_GRUP_OCUP=periodo.CO_GRUP_OCUP 
+inner join TTPUES_TRAB as puesto on puesto.CO_EMPR=empresa.CO_EMPR and puesto.CO_PUES_TRAB=periodo.CO_PUES_TRAB 
+inner join TMTRAB_EMPR as trab on trab.CO_TRAB=emp.CO_TRAB and puesto.CO_PUES_TRAB=periodo.CO_PUES_TRAB 
+where emp.CO_TRAB=@p0
+order by trab.FE_INGR_EMPR desc ;";
+            try
+            {
+                using (var con = new SqlConnection(_conexion))
+                {
+                    con.Open();
+                    var query = new SqlCommand(consulta, con);
+                    query.Parameters.AddWithValue("@p0", dni);
+                    using (var dr = query.ExecuteReader())
+                    {
+                        if (dr.HasRows)
+                        {
+                            while (dr.Read())
+
+                            {
+                                persona.CO_TRAB = ManejoNulos.ManageNullStr(dr["CO_TRAB"]);
+                                persona.NO_TRAB = ManejoNulos.ManageNullStr(dr["NO_TRAB"]);
+                                persona.NO_APEL_PATE = ManejoNulos.ManageNullStr(dr["NO_APEL_PATE"]);
+                                persona.NO_APEL_MATE = ManejoNulos.ManageNullStr(dr["NO_APEL_MATE"]);
+                                persona.TI_SITU = ManejoNulos.ManageNullStr(dr["TI_SITU"]);
+                                persona.DE_NOMB = ManejoNulos.ManageNullStr(dr["DE_NOMB"]);
+                                persona.DE_UNID = ManejoNulos.ManageNullStr(dr["DE_UNID"]);
+                                persona.DE_SEDE = ManejoNulos.ManageNullStr(dr["DE_SEDE"]);
+                                persona.DE_DEPA = ManejoNulos.ManageNullStr(dr["DE_DEPA"]);
+                                persona.DE_AREA = ManejoNulos.ManageNullStr(dr["DE_AREA"]);
+                                persona.DE_GRUP_OCUP = ManejoNulos.ManageNullStr(dr["DE_GRUP_OCUP"]);
+                                persona.DE_PUES_TRAB = ManejoNulos.ManageNullStr(dr["DE_PUES_TRAB"]);
+                                persona.NU_TLF1 = ManejoNulos.ManageNullStr(dr["NU_TLF1"]);
+                                persona.NO_DIRE_TRAB = ManejoNulos.ManageNullStr(dr["NO_DIRE_TRAB"]);
+                                persona.NU_RUCS = ManejoNulos.ManageNullStr(dr["NU_RUCS"]);
+                                persona.CO_EMPR = ManejoNulos.ManageNullStr(dr["CO_EMPR"]);
+                                persona.FE_CESE_TRAB = ManejoNulos.ManageNullStr(dr["FE_CESE_TRAB"]);
+                                persona.FE_INGR_EMPR = ManejoNulos.ManageNullStr(dr["FE_INGR_EMPR"]);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                persona = new PersonaSqlEntidad();
+            }
+
+            return persona;
+        }
     }
 }
